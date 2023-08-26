a334c48b3a994a95501fc65d44e0909cef8314c9
tparizoda <parizodatogaeva@gmail.com>
2023-08-26 09:15:22 -0700

    spacesfixed

--- Entities/Quiz.cs
+++ Entities/Quiz.cs
@@ -4,13 +4,13 @@ public class Quiz
     public Guid OwnerId {get;set;}
     public Guid Id { get; set; }
     public string Title {get;set;}
-    public string Description {get;set;}
+    public string Description { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime ModifiedAt { get; set; }
     public DateTime OpeningDateTime { get; set; }
     public DateTime ClosingDateTime { get; set; }
     public EQuizStatus Status { get; set; }
-    public bool IsPublic {get;set;}
+    public bool IsPublic { get; set; } 
     public string[] Tags { get; set; }
     public string Code { get; set; }
     public List<QuizQuestion> Questions { get; set; }
