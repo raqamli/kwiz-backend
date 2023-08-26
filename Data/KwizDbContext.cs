53323475bf66a4b7822134dfb3b2502cf8056011
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-08-26 23:12:34 +0900

    Minor changes

--- Data/KwizDbContext.cs
+++ Data/KwizDbContext.cs
@@ -17,35 +17,19 @@ public class KwizDbContext : DbContext, IKwizDbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        UpdateDates(ChangeTracker.Entries<IHasTime>().Where(e => e.State == EntityState.Modified));
-        return base.SaveChangesAsync(cancellationToken);
-    }
+        => base.SaveChangesAsync(cancellationToken);
 
     private void OnStateChanged(object sender, EntityStateChangedEventArgs e)
     {
         if (e.Entry.Entity is IHasTime hasTimestamp)
-            if (e.NewState == EntityState.Added)
+            if(e.NewState == EntityState.Added)
+            {
                 hasTimestamp.CreatedAt = DateTime.UtcNow;
-            else if (e.NewState == EntityState.Modified)
                 hasTimestamp.UpdatedAt = DateTime.UtcNow;
-
-        if (e.Entry.Entity is IHasTime timeStamp)
-            if (e.NewState == EntityState.Added)
-                timeStamp.CreatedAt = DateTime.UtcNow;
-            else if (e.NewState == EntityState.Modified)
-                timeStamp.UpdatedAt = DateTime.UtcNow;
-    }
-
-    private void UpdateDates(IEnumerable<EntityEntry<IHasTime>> entries)
-    {
-        if (entries.Any() is false)
-            return;
-
-        foreach (var entry in entries)
-        {
-            entry.Entity.UpdatedAt = DateTime.UtcNow;
-        }
+            }
+            else if(e.NewState == EntityState.Modified)
+                hasTimestamp.UpdatedAt = DateTime.UtcNow;
     }
 }
