57385cd63519d4bd9574fb11b349ee0f954148e9
Mehriddin <71074521+mekhriddinbozorov@users.noreply.github.com>
2023-08-26 19:47:09 +0500

    CU-860rm60v2 - Add PUT for user interests controller (#2)

--- Controllers/UserinfoController.cs
+++ Controllers/UserinfoController.cs
@@ -22,13 +22,13 @@ public class UserinfoController : ControllerBase
         var userId = Guid.Parse(idCliam);
         var interests = await dbContext.TechInterests
             .FirstOrDefaultAsync(t => t.UserId == userId, cancellationToken);
-        
-        if(interests is null)
+
+        if (interests is null)
             return NoContent();
 
         return Ok(new GetTechInterestDto(interests));
     }
-    
+
     [Authorize]
     [HttpPost("interests")]
     public async Task<IActionResult> CreateInterest(
@@ -36,14 +36,14 @@ public class UserinfoController : ControllerBase
         [FromBody] IEnumerable<string> userSelectedInterests,
         CancellationToken cancellationToken = default)
     {
-        if(userSelectedInterests.Any() is false)
+        if (userSelectedInterests.Any() is false)
             return BadRequest("User must select at least one interest.");
 
         var idCliam = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
         var userId = Guid.Parse(idCliam);
         var interests = await dbContext.TechInterests
             .FirstOrDefaultAsync(t => t.UserId == userId, cancellationToken);
-        
+
         if (interests is not null)
             return Conflict("User already has tech interests selected.");
 
@@ -55,5 +55,29 @@ public class UserinfoController : ControllerBase
         await dbContext.SaveChangesAsync(cancellationToken);
 
         return CreatedAtAction(nameof(GetUserInterests), new GetTechInterestDto(persistedInterests.Entity));
+    }
+
+    [Authorize]
+    [HttpPut("interests")]
+    public async Task<IActionResult> UpdateInterest(
+        [FromServices] IKwizDbContext dbContext,
+        [FromBody] IEnumerable<string> userSelectedInterests,
+        CancellationToken cancellationToken = default)
+    {
+        if (userSelectedInterests.Any() is false)
+            return BadRequest("User must select at least one interest.");
+
+        var idCliam = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var userId = Guid.Parse(idCliam);
+        var interests = await dbContext.TechInterests
+            .FirstOrDefaultAsync(t => t.UserId == userId, cancellationToken);
+
+        if (interests is null)
+            BadRequest("User has never selected interests before.");
+        
+        interests.Interests = userSelectedInterests.ToArray();
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Ok(new GetTechInterestDto(interests));
     }
 }
