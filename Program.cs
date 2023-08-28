74b597ab94169f4a152e8ba57ba85f6d32403736
Mehriddin <mr.mekhriddin@gmail.com>
2023-08-28 21:31:08 +0500

    CU-860rm602y - Add Jaeger Tracing

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
 using System.Text.Json.Serialization;
+using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders.Thrift;
 using Microsoft.EntityFrameworkCore;
 using Kwiz.Api;
 using Kwiz.Api.Data;
+using OpenTracing;
+using OpenTracing.Contrib.NetCore.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<IKwizDbContext, KwizDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+builder.Services.AddDbContext<IKwizDbContext, KwizDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 builder.Services.AddSwaggerDocumentation(builder.Configuration);
 builder.Services.AddKeycloakAuthenticationServices(builder.Configuration);
 builder.Services.AddControllers()
     .AddJsonOptions(config => config.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddOpenTracing();
+builder.Services.AddSingleton<ITracer>(sp =>
+{
+    var serviceName = sp.GetRequiredService<IWebHostEnvironment>().ApplicationName;
+    var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+    var reporter = new RemoteReporter.Builder().WithLoggerFactory(loggerFactory).WithSender(new UdpSender())
+        .Build();
+    var tracer = new Tracer.Builder(serviceName)
+        .WithSampler(new ConstSampler(true))
+        .WithReporter(reporter)
+        .Build();
+    return tracer;
+});
+
+builder.Services.Configure<HttpHandlerDiagnosticOptions>(options =>
+    options.OperationNameResolver =
+        request => $"{request.Method.Method}: {request?.RequestUri?.AbsoluteUri}");
 
 var app = builder.Build();
 
